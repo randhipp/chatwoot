99fc15442355f3920dc336959ced9b2efd070426
Randhi Pratama Putra <randhi.pp@gmail.com>
2021-08-29 11:14:24 +0700

    Merge commit '20b96c7c60409765341d9e67d4a9956587aa45f1'

--- app/javascript/dashboard/routes/dashboard/contacts/components/ContactsTable.vue
+++ app/javascript/dashboard/routes/dashboard/contacts/components/ContactsTable.vue
@@ -222,7 +222,7 @@ export default {
           profiles: additional.social_profiles || {},
           city: additional.city || '---',
           country: additional.country || '---',
-          conversations_count: item.conversations_count || '---',
+          conversationsCount: item.conversations_count || '---',
           last_activity_at: lastActivityAt
             ? this.dynamicTime(lastActivityAt)
             : '---',
